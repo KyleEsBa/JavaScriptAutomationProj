31d39e4c6a3229a32d0efca6235571722d910dd7
KyleEsBa <kyleb.-95@hotmail.com>
2024-12-10 17:37:38 -0800

    Fixtures

---
+++ tests/Fixtures.ts
@@ -0,0 +1,14 @@
+import { test as base } from '@playwright/test';
+import {setTimeout} from "timers/promises";
+
+export const test = base.extend<{ forEachTest: void }>({
+  forEachTest: [async ({ page }, use) => {
+    // This code runs before every test.
+    //await page.goto('https://www.google.com/');
+    await page.goto('');    //Automatically calls the baseURL from the playwrigth.config.js
+    await use();
+    // This code runs after every test.
+    await setTimeout(5000);
+    await page.close();
+  }, { auto: true }],  // automatically starts for every test.
+});
--- tests/search.spec.js
+++ tests/search.spec.js
@@ -1,12 +1,10 @@
-import {test, expect} from '@playwright/test';
+const {expect} = require ('@playwright/test');
+import {test} from './Fixtures';
 //import { base } from '../utilities/fixtures';
 //import {test, expect} from '@playwright/test';
 const {GoogleSearchPage}=require('../pages/GoogleSearchPage');
-import {setTimeout} from "timers/promises";
 
 test('Google search', async({page})=>{
-    //await page.goto('https://www.google.com/');
-    await page.goto('');    //Automatically calls the baseURL from the playwrigth.config.js
     const pageTitle=page.title();
     await expect(page).toHaveTitle('Google');
     console.log('Page title is: ', pageTitle);
@@ -16,7 +14,4 @@ test('Google search', async({page})=>{
     await googleSearchPage.searchTbx.fill('ChromeDriver');
     await page.press('body','Enter');
     await googleSearchPage.searchedOptionLnk.click();
-    await setTimeout(5000);
-
-    await page.close();
 });
