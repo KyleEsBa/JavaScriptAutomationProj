13e8d30ef6016f6664f07168dc9bf5e7dddf50aa
KyleEsBa <kyleb.-95@hotmail.com>
2025-08-24 19:11:08 -0400

    Updating locator

--- pages/AmazonSearchPage.js
+++ pages/AmazonSearchPage.js
@@ -3,7 +3,7 @@ const {expect}=require('@playwright/test');
 class AmazonSearchPage{
     constructor(page){
         this.page=page;
-        this.searchTbx=page.locator("xpath=//*[@role='searchbox']");
+        this.searchTbx=page.locator("xpath=//*[@title='Search For']");
         this.searchedOptionLnk=page.locator("xpath=//span[contains(text(), 'results for')]//following-sibling::span[contains(text(),'Shoes')]");
     }
 }
